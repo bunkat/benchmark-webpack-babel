1d3a0a5df041b4d96ac503f946a0544b88e520e9
Bill Scheidel <bill@bunkat.com>
2015-08-27 11:29:24 -0700

    fixed dependencies

--- getWebpackConfig.js
+++ getWebpackConfig.js
@@ -26,70 +26,12 @@ module.exports = function(options) {
           test: /\.js$/,
           loaders: ['babel?stage=0'],
           include: path.join(__dirname, 'src/js')
-        },
-        { 
-          test: /\.css$/,
-          loader: 'style-loader!css-loader',
-          include: path.join(__dirname, 'src/css')
-        },
-        {
-          test: /\.json$/,
-          loaders: ['json']
-        },
-        {
-          test: /\.(png|jpg)$/,
-          loader: 'url-loader?limit=8192'
         }
       ]
     },
   };
 
-  if (options.environment === 'dev') {
-    _.assign(config, {
-      devtool: 'eval-cheap-module-source-map',
-      entry: [
-        'webpack-dev-server/client?http://192.168.1.106:8080',
-        'webpack/hot/only-dev-server',
-        './src/js/main.js'
-      ],
-      plugins: [
-/*        new webpack.DefinePlugin({
-          "process.env": {
-            NODE_ENV: JSON.stringify("production")
-          }
-        }),*/
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-      ],
-      module: {
-        noParse: [
-          /\/sinon.js/
-        ],
-        loaders: [
-          {
-            test: /\.js$/,
-            loaders: ['react-hot', 'babel?stage=0'],
-            include: path.join(__dirname, 'src/js')
-          },
-          { 
-            test: /\.css$/,
-            loader: 'style-loader!css-loader',
-            include: path.join(__dirname, 'src/css')
-          },
-          {
-            test: /\.json$/,
-            loaders: ['json']
-          },
-          {
-            test: /\.(png|jpg)$/,
-            loader: 'url-loader?limit=8192'
-          }
-        ]
-      },
-    });
-  }
-
-  else if (options.environment === 'benchmark') {
+  if (options.environment === 'benchmark') {
     _.assign(config, {
       devtool: 'eval-cheap-module-source-map',
       output: {
